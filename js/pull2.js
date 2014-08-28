5c900bf0699a3df39d744d1bc8eca5a0aaed3b79
Lamon phillips <lamonphillips6@gmail.com>
2014-08-28 22:26:24 +0000

    initial commit

--- js/pull2.js
+++ js/pull2.js
@@ -2,34 +2,34 @@ $.getJSON('js/busstops.json', function(busstops){
   $('#busstoplist').empty();
   
   $.each(busstops, function(i,busstop){
-      $('#busstoplist').append(generateBusstop(busstop));
+      $('#busstoplist').append(generateBusstopLink(busstop));
    });
    
    $('#busstoplist').listview('refresh');
    
 });
 
-function generateBusstop(busstop){
+function generateLStopLink(busstop){
 
     return'<li><a href="javascript:void(0)'
            + '"onclick="goToStopDetailPage(\''
            + busstop.stop_name
            +'\',\''
-           + busstop.stop_descriptive_name +'\')">'
-           + busstop._stop_name
+           + busstop.stop_desc +'\')">'
+           + busstop.stop_name
            + '</a></li>';
 }
 
 function goToStopDetailPage(busstopName){
 
     //create the page html template
-   var stopPage =$("<div data-role='page' data-url=station><div data-role='header'><h1>"
-                  + stopName + "</h1></div><div data-role='content'></div><div data-role='footer'><h4>"
-                  + stopName + "</h4></div></div>");
+   var busstopPage =$("<div data-role='page' data-url=station><div data-role='header'><h1>"
+                  + busstopName + "</h1></div><div data-role='content'></div><div data-role='footer'><h4>"
+                  + busstopName + "</h4></div></div>");
                   
 //append the new page to the page container
-stopPage.appendTo( $.mobile.pageContainer );
+busstopPage.appendTo( $.mobile.pageContainer );
 
 //go to the newly create page
-$.mobile.changePage(stopPage);
+$.mobile.changePage(busstopPage);
 }
